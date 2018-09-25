0ef650a317883aa0471e38b3ffd23e8855932239
Laurence Roberts <lsjroberts@outlook.com>
2018-09-25 09:19:14 +0100

    Update sandbox to return promise on listen

--- workspaces/program/src/sandbox.ts
+++ workspaces/program/src/sandbox.ts
@@ -12,34 +12,38 @@ export default function sandbox({ router }: SandboxProgramOptions): Program {
   const compiledRoutes = compile(router);
 
   return (port: number) => {
-    http
-      .createServer((req, res) => {
-        const { headers, method, url } = req;
-        let chunks = [];
+    const server = http.createServer((req, res) => {
+      const { headers, method, url } = req;
+      let chunks = [];
 
-        req
-          .on('error', err => {
+      req
+        .on('error', err => {
+          // TODO: do nice error
+          console.error(err);
+        })
+        .on('data', chunk => {
+          chunks.push(chunk);
+        })
+        .on('end', () => {
+          const body = Buffer.concat(chunks).toString();
+
+          res.on('error', err => {
             // TODO: do nice error
             console.error(err);
-          })
-          .on('data', chunk => {
-            chunks.push(chunk);
-          })
-          .on('end', () => {
-            const body = Buffer.concat(chunks).toString();
-
-            res.on('error', err => {
-              // TODO: do nice error
-              console.error(err);
-            });
-
-            const route = resolveRoute(compiledRoutes, req);
-            route(req, res);
-            res.end();
           });
-      })
-      .listen(port);
-    console.log(`Started server at http://localhost:${port}`);
+
+          const route = resolveRoute(compiledRoutes, req);
+          route(req, res);
+          res.end();
+        });
+    });
+
+    return new Promise(resolve => {
+      server.listen(port, () => {
+        console.log(`Started server at http://localhost:${port}`);
+        resolve();
+      });
+    });
   };
 }
 
